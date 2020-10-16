a3603af247aa63989c0e35c8180db0978e181b5d
and1801 <and1801@gmail.com>
2020-10-16 16:31:50 +0900

    StudentInfo

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Andrey Kolesnikov");
-        System.out.println("18.01.1984");
+        System.out.println("18.01.84");
     }
 }
